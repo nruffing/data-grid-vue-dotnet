8606cd6d59b578aa0c2c836eed58341fb711a259
Nicholas Ruffing <nick.autoforms@gmail.com>
2023-11-18 14:11:04 -0500

    add comments to swagger page

--- DataGridVueDotnetExample/Program.cs
+++ DataGridVueDotnetExample/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using DataGridVueDotnetExample.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,7 +18,26 @@ namespace DataGridVueDotnetExample
       builder.Services.AddControllers();
       // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
       builder.Services.AddEndpointsApiExplorer();
-      builder.Services.AddSwaggerGen();
+      builder.Services.AddSwaggerGen(options => {
+        options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+        {
+          Version = "v1",
+          Title = "Data Grid Vue Example API",
+          Description = "Example API demonstrating how to implement server-side paging, sorting, filtering and grid state storage when using data-grid-vue and data-grid-vue-dotnet.",
+          Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+          {
+            Name = "Nicholas Ruffing",
+            Email = "nicholasruffing70@gmail.com",
+          },
+          License = new Microsoft.OpenApi.Models.OpenApiLicense()
+          {
+            Name = "MIT",
+            Url = new Uri("https://github.com/nruffing/data-grid-vue-dotnet/blob/main/LICENSE"),
+          }
+        });
+        options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "DataGridVueDotnet.xml"));
+        options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, Assembly.GetExecutingAssembly().GetName().Name + ".xml"), true);
+      });
 
       builder.Services.AddDbContext<TestContext>(optionsBuilder =>
       {
@@ -43,12 +63,11 @@ namespace DataGridVueDotnetExample
 
       app.UseCors("AllowLocal");
 
-      // Configure the HTTP request pipeline.
-      if (app.Environment.IsDevelopment())
-      {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-      }
+      app.UseSwagger();
+      app.UseSwaggerUI(options => {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = "";
+      });
 
       app.UseHttpsRedirection();
 
